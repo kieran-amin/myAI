609430ec03e0b696f798f8ce29c7f460b3269035
kieran-amin <112574793+kieran-amin@users.noreply.github.com>
2025-03-06 19:14:04 -0500

    Update messages.tsx

--- components/chat/messages.tsx
+++ components/chat/messages.tsx
@@ -1,6 +1,6 @@
 import { DisplayMessage } from "@/types";
 import { motion } from "framer-motion";
-import Image from "./Chatbot.png";
+import Image from "next/image";
 import { Formatting } from "./formatting";
 import { LoadingIndicator } from "@/types";
 import Loading from "./loading";
@@ -9,7 +9,7 @@ import { AI_NAME } from "@/configuration/identity";
 function AILogo() {
   return (
     <div className="w-9 h-9">
-      <Image src="/ai-logo.png" alt={AI_NAME} width={36} height={36} />
+      <Image src="/Chatbot.png" alt={AI_NAME} width={36} height={36} />
     </div>
   );
 }
