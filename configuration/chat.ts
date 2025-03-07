7e317423ece86b8462ee7c67e03af6f79561b866
Kieran Amin <kieran1216@gmail.com>
2025-03-07 14:52:04 -0500

    updated visual

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,6 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
-export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant. What can I help you with? I can answer questions about Kieran's resume and projects, or help with general questions about computer science.`;
+export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s AI assistant. What can I help you with? I can answer questions about Kieran's resume and projects, or answer questions about UNC Football or Basketball.`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I'm having trouble generating a response. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,6 +1,6 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Start typing to interact with the bot!`;
+export const CHAT_HEADER: string = `Start typing to interact with Kieran Bot!`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Kieran Amin @ UNC`;
 export const CLEAR_BUTTON_TEXT: string = `Clear chat history`;
